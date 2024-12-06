1f6985920b7b34f3bbee04a500967b62c183035c
Simon_Webber <simon.webber02@gmail.com>
2024-12-06 15:38:21 -0500

    began creation of model input grid and sat data tranform to grid

---
+++ gridproj.py
@@ -0,0 +1,63 @@
+# 51N/-131E, 
+# 51N/-66E, 
+# 23N/-66E, 
+# 23N/-131E
+
+import numpy as np
+from pyproj import CRS, Transformer
+from cmipc_data_retrieval import *
+from noaa_calculate_degrees_func import calculate_degrees
+
+# Define the Geographic CRS (WGS 84 - EPSG:4326)
+geographic_crs = CRS.from_epsg(4326)
+
+# Define the Albers Equal-Area Projection
+# Example for Europe: central meridian at 10° longitude, and two standard parallels at 20°N and 50°N
+albers_crs = CRS.from_proj4("+proj=aea +lat_1=23 +lat_2=51 +lon_0=-96 +datum=WGS84")
+
+# Create a transformer
+transformer = Transformer.from_crs(geographic_crs, albers_crs, always_xy=True)
+lat_max =  51  # deg N
+lat_min =  23  # deg N
+lon_max = -66  # deg E
+lon_min = -131 # deg E 
+
+# Define the bounds for the grid - Transform lat and lon bounds
+x_min, y_min = transformer.transform(lon_min, lat_min)
+x_max, y_max = transformer.transform(lon_max, lat_max)
+
+sat_data = retrieve_s3_data(get_range_CMIPC_data_fileKeys(2024, 311, 23, 59, 14)[0])
+data_temps = sat_data.variables["CMI"][:]
+lat, lon = calculate_degrees(sat_data)
+lat      = lat.flatten('C')
+lon      = lon.flatten('C')
+data_temps = data_temps.flatten()
+
+# Transform latitudes and longitudes into Albers Equal-Area projection
+x, y = transformer.transform(lon, lat)
+
+# Define grid parameters (grid spacing and bounding box)
+grid_spacing = 2000  # Grid cell size (2 km per cell)
+
+# Make the grid we will dump everything into:
+# Create the grid (x and y grid points)
+x_grid = np.arange(x_min, x_max, grid_spacing)
+y_grid = np.arange(y_min, y_max, grid_spacing)
+
+# Create an empty data array (grid) with the same shape as the grid
+data_array = np.zeros((len(y_grid), len(x_grid)))
+
+print(f"{x.shape=}")
+print(f"{y.shape=}")
+print(f"{data_temps.shape=}")
+
+# Map the intensities to the grid
+for i, (xi, yi) in enumerate(zip(x, y)):
+    row_idx = np.argmin(np.abs(y_grid - yi))
+    col_idx = np.argmin(np.abs(x_grid - xi))
+    data_array[row_idx, col_idx] = data_temps[i]
+
+# Print the resulting grid (NumPy array)
+print(data_array.shape)
+print(data_array)
+
--- viz.py
+++ viz.py
@@ -4,39 +4,12 @@ from cartopy import crs as ccrs
 from goes2go.tools import abi_crs
 import xarray as xr
 from cmipc_data_retrieval import *
+from netCDF4 import Dataset
 import numpy as np
+import pyproj
+from noaa_calculate_degrees_func import calculate_degrees
 
-def calculate_degrees(file_id):
-    
-    # Read in GOES ABI fixed grid projection variables and constants
-    x_coordinate_1d = file_id.variables['x'][:]  # E/W scanning angle in radians
-    y_coordinate_1d = file_id.variables['y'][:]  # N/S elevation angle in radians
-    projection_info = file_id.variables['goes_imager_projection']
-    lon_origin = projection_info.longitude_of_projection_origin
-    H = projection_info.perspective_point_height+projection_info.semi_major_axis
-    r_eq = projection_info.semi_major_axis
-    r_pol = projection_info.semi_minor_axis
-    
-    # Create 2D coordinate matrices from 1D coordinate vectors
-    x_coordinate_2d, y_coordinate_2d = np.meshgrid(x_coordinate_1d, y_coordinate_1d)
-    
-    # Equations to calculate latitude and longitude
-    lambda_0 = (lon_origin*np.pi)/180.0  
-    a_var = np.power(np.sin(x_coordinate_2d),2.0) + (np.power(np.cos(x_coordinate_2d),2.0)*(np.power(np.cos(y_coordinate_2d),2.0)+(((r_eq*r_eq)/(r_pol*r_pol))*np.power(np.sin(y_coordinate_2d),2.0))))
-    b_var = -2.0*H*np.cos(x_coordinate_2d)*np.cos(y_coordinate_2d)
-    c_var = (H**2.0)-(r_eq**2.0)
-    r_s = (-1.0*b_var - np.sqrt((b_var**2)-(4.0*a_var*c_var)))/(2.0*a_var)
-    s_x = r_s*np.cos(x_coordinate_2d)*np.cos(y_coordinate_2d)
-    s_y = - r_s*np.sin(x_coordinate_2d)
-    s_z = r_s*np.cos(x_coordinate_2d)*np.sin(y_coordinate_2d)
-    
-    # Ignore numpy errors for sqrt of negative number; occurs for GOES-16 ABI CONUS sector data
-    np.seterr(all='ignore')
-    
-    abi_lat = (180.0/np.pi)*(np.arctan(((r_eq*r_eq)/(r_pol*r_pol))*((s_z/np.sqrt(((H-s_x)*(H-s_x))+(s_y*s_y))))))
-    abi_lon = (lambda_0 - np.arctan(s_y/(H-s_x)))*(180.0/np.pi)
-    
-    return abi_lat, abi_lon
+
 
 projection = ccrs.AlbersEqualArea(
     central_longitude=-96,
@@ -45,7 +18,7 @@ projection = ccrs.AlbersEqualArea(
 )
 
 fig = plt.figure(figsize=(15, 12))
-ax_east = fig.add_subplot(1, 2, 2, projection=projection)
+ax_east = fig.add_subplot(1, 2, 1, projection=projection)
 ax_east_2 = fig.add_subplot(1, 2, 2, projection=projection)
 
 
@@ -53,22 +26,53 @@ data = retrieve_s3_data(get_range_CMIPC_data_fileKeys(2024, 311, 23, 59, 14)[0])
 print("**************************************************************************************************")
 print(type(data))
 lat,lon = calculate_degrees(data)
+print(f"{type(lat)=}")
 dataset = xr.open_dataset(xr.backends.NetCDF4DataStore(data))
 print(type(dataset))
 print("**************************************************************************************************")
 
 crs, x, y = abi_crs(dataset, reference_variable="CMI")
-print(len(x))
-print(crs)
-print(dir(crs))
-print(type(crs))
+print(f"{len(x) = }")
+# print(crs)
+# print(dir(crs))
+# print(type(crs))
 ax_east.coastlines(resolution="50m")
+
+print(type(x))
+
+latData = np.ma.getdata(lat)
+latMask = np.ma.getmaskarray(lat)
+latData[latMask] = 100
+
+lonData = np.ma.getdata(lon)
+lonMask = np.ma.getmaskarray(lon)
+lonData[lonMask] = 100
+
 ax_east.pcolormesh(x, y, data["CMI"], transform=crs)
 
 ax_east_2.coastlines(resolution="50m")
-print(len(lat))
-ax_east_2.pcolormesh(lat, lon, data["CMI"])
-plt.show()
+# print(len(lat))
+# print("Dir of lat is ", dir(lat))
+# print(type(lat))
+ax_east_2.pcolormesh(lonData, latData, data["CMI"])
+
+
+
+
+print(f"{lon.shape = }")
+print(lon)
+
+
+# plt.show()
+
+# nc_dataset = Dataset('in_memory.nc', mode='r', memory=data_io.read())
+# noaa_orig = Dataset("./goes16_abi_conus_lat_lon/goes16_abi_conus_lat_lon.nc", 'r')
+# noaa_interpolated = Dataset("./goes16_abi_conus_interpolated_lat_lon/goes16_abi_conus_interpolated_lat_lon.nc", 'r')
+# noaa_orig = noaa_orig.variables["latitude"][:]
+# noaa_interpolated = noaa_interpolated.variables["latitude"][:]
+# print(noaa_orig[np.ma.getmaskarray(noaa_orig)])
+# print(noaa_interpolated[np.ma.getmaskarray(noaa_orig)])
+
 
 # fig = plt.figure(figsize=(15, 12))
 # ax_east = fig.add_subplot(1, 2, 2, projection=projection)
