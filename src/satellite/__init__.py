c03447fbcefe7e1d83fbc96ae96ff416fe2e363f
Tanay Nistala <tanay.nistala@tufts.edu>
2025-02-10 10:27:10 -0500

    Fixed some errors in conversion

--- src/satellite/__init__.py
+++ src/satellite/__init__.py
@@ -12,6 +12,7 @@ def fetch(timestamp: dt.datetime, satellite: GOES) -> Dataset:
 def fetch_band(data: Dataset, band: int) -> DataArray:
     return data[f"CMI_C{band:02d}"].data
 
+
 # Code pulled from the NOAA Documentation for the AWS Bucket
 def calculate_coordinates(data: Dataset) -> tuple[npt.ArrayLike, npt.ArrayLike]:
     # Read in GOES ABI fixed grid projection variables and constants
@@ -28,6 +29,9 @@ def calculate_coordinates(data: Dataset) -> tuple[npt.ArrayLike, npt.ArrayLike]:
 
     # Create 2D coordinate matrices from 1D coordinate vectors
     x_coordinate_2d, y_coordinate_2d = np.meshgrid(x_coordinate_1d, y_coordinate_1d)
+
+    # Ignore numpy errors for sqrt of negative number; occurs for GOES-16 ABI CONUS sector data
+    np.seterr(all="ignore")
 
     # Equations to calculate latitude and longitude
     lambda_0 = (lon_origin * np.pi) / 180.0
@@ -47,9 +51,6 @@ def calculate_coordinates(data: Dataset) -> tuple[npt.ArrayLike, npt.ArrayLike]:
     s_x = r_s * np.cos(x_coordinate_2d) * np.cos(y_coordinate_2d)
     s_y = -r_s * np.sin(x_coordinate_2d)
     s_z = r_s * np.cos(x_coordinate_2d) * np.sin(y_coordinate_2d)
-
-    # Ignore numpy errors for sqrt of negative number; occurs for GOES-16 ABI CONUS sector data
-    np.seterr(all="ignore")
 
     abi_lat = (180.0 / np.pi) * (
         np.arctan(
@@ -79,7 +80,6 @@ def project(
     # Map to grid coordinates
     rows = convert(lat[mask], "LAT")
     cols = convert(lon[mask], "LON")
-    print(temp.shape, mask.shape)
     temps = temp[mask]
 
     # Shape data into grid
@@ -88,8 +88,10 @@ def project(
 
     return data
 
+
 def smooth(data: npt.ArrayLike | npt.DTypeLike) -> npt.ArrayLike | npt.DTypeLike:
     from scipy.ndimage import distance_transform_edt
+
     empty_mask = data <= 0
     filled_data = data.copy()
     indices = distance_transform_edt(
@@ -99,5 +101,8 @@ def smooth(data: npt.ArrayLike | npt.DTypeLike) -> npt.ArrayLike | npt.DTypeLike
 
     return filled_data
 
-def union_sat_data(west: npt.ArrayLike | npt.DTypeLike, east: npt.ArrayLike | npt.DTypeLike) -> npt.ArrayLike | npt.DTypeLike:
+
+def union_sat_data(
+    west: npt.ArrayLike | npt.DTypeLike, east: npt.ArrayLike | npt.DTypeLike
+) -> npt.ArrayLike | npt.DTypeLike:
     pass
