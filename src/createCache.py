108e22ec5c45fd95e51b1f1d26cf765d0bb63520
Aiden Auretto <aiden.auretto@tufts.edu>
2025-03-08 18:29:31 -0500

    Reading from CSV for cache

--- src/createCache.py
+++ src/createCache.py
@@ -43,10 +43,50 @@ def create_cache(start, end):
     })
     df.to_csv(CSV_FANME, mode = "a", header=False, index=False)
 
+# parses all nones into actual stuff 
+def parse_negs():
+    ...
+
+def pull_from_cache(start, end):
+    # Reports has: (idx, val) OR (None, report)
+    reports = []
+
+    for chunk in pd.read_csv(CSV_FANME, chunksize=10):
+        # Look at last ts and if its before start continue
+        if pd.to_datetime(chunk['Timestamp'].iloc[len(chunk) - 1]).to_pydatetime() < start:
+            print("CONTINUING, last elem < Start")
+            continue
+        
+        # Look at first ts and if its after end break
+        if pd.to_datetime(chunk['Timestamp'].iloc[0]).to_pydatetime() > end:
+            print("BREAKING, first elem > END")
+            break
+        
+        # Read entire chunk if it is in range:
+        if pd.to_datetime(chunk['Timestamp'].iloc[0]).to_pydatetime() > start and \
+           pd.to_datetime(chunk['Timestamp'].iloc[len(chunk) - 1]).to_pydatetime() < end:
+            reports += chunk['Data'].tolist()
+        
+        else: # find latest endpoint before end
+            start_idx = None
+            end_idx   = len(chunk)
+            for index, row in chunk.iterrows():
+                # TODO: ask Tanay if we are incl or excl on the pirep time ranges
+                if pd.to_datetime(row['Timestamp']).to_pydatetime() > start and start_idx is None:
+                    start_idx = index
+                if pd.to_datetime(row['Timestamp']).to_pydatetime() > end:
+                    print("in end case: ", pd.to_datetime(row['Timestamp']).to_pydatetime())
+                    end_idx   = index
+                    break
+            print(f"slicing: [{start_idx}:{end_idx}]")
+            reports += chunk['Data'].iloc[start_idx:end_idx].tolist()
+        
+    return reports
+        
+
+
 if __name__ == "__main__":
     if '--load' not in sys.argv:
-        start = dt.datetime(2024, 11, 7,  0,  0, 0) 
-        end   = dt.datetime(2024, 11, 7,  0, 30, 0) 
         # Intialize csv for appending later
         if '--init' in sys.argv:
             df = pd.DataFrame({
@@ -54,14 +94,19 @@ if __name__ == "__main__":
                 "Data"      : []
             })
             df.to_csv(CSV_FANME, mode = "w", header=True, index=False)
-        create_cache(start, end)
+        else:
+            for hr in range(5):
+                start = dt.datetime(2024, 11, 7,  0,  0, 0) 
+                end   = dt.datetime(2024, 11, 7,  0, 30, 0) 
+                diff = dt.timedelta(hours=hr)
+                create_cache(start + diff, end + diff)
 
-        df = pd.read_csv(CSV_FANME)
-        print(df)
+            df = pd.read_csv(CSV_FANME)
     else:
-        df = pd.read_csv(CSV_FANME)
-        for i in range(len(df)):
-            time = pd.to_datetime(df['Timestamp'].iloc[i]).to_pydatetime()
-            print(f"Report at time {time} of type {type(time)}")
+        start = dt.datetime(2024, 11, 7, 0, 2)
+        end = dt.datetime(2024, 11, 7, 0, 5)
+        reports = pull_from_cache(start, end)
+        df = pd.DataFrame({'Data': reports})
+        df.to_csv('/skyblue/res.csv')
 
     
