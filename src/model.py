43f94cc7c374221c82a2abf7af42b3b401fd28a6
Tanay Nistala <tanay.nistala@tufts.edu>
2025-03-27 12:55:53 -0400

    Reworked the hyperparameter tuning architecture slightly

--- src/model.py
+++ src/model.py
@@ -3,14 +3,10 @@ import multiprocessing
 
 import keras
 import keras_tuner as kt
-import numpy as np
 from numpy import random as rnd
 from goes2go import GOES
-from numpy import typing as npt
 
 import consts as consts
-import pirep as pr
-import satellite as st
 from generator import Generator
 
 
@@ -37,34 +33,36 @@ def generate_timestamps(
     return timestamps
 
 
-def model_initializer(hp, frame_size: int = 9):
-    # Model parameters
-    num_classes = 14
-    lat_size = consts.GRID_RANGE["LAT"]
-    lon_size = consts.GRID_RANGE["LON"]
-
+def build_model(
+    hp: kt.HyperParameters,
+    dim_frames=9,
+    dim_lat=consts.GRID_RANGE["LAT"],
+    dim_lon=consts.GRID_RANGE["LAT"],
+    dim_alt=consts.GRID_RANGE["ALT"],
+    dim_bands=6,
+):
     model = keras.Sequential()
-    model.add(keras.layers.Input((frame_size, lat_size, lon_size, 6))) # 6 is number of bands 
+    model.add(keras.layers.Input((dim_frames, dim_lat, dim_lon, dim_bands)))
+
+    # 2D Convolutional LSTM layer
+    hp_filters = hp.Choice("filters", values=[16, 32, 64])
     model.add(
         keras.layers.ConvLSTM2D(
-            filters=hp.Int("filters", min_value=16, max_value=64, step=16),
+            filters=hp_filters,
             kernel_size=(3, 3),
             padding="same",
             return_sequences=True,
         )
     )
 
-    # Dropout for regularization
-    model.add(
-        keras.layers.Dropout(
-            rate=hp.Float("dropout_rate", min_value=0.2, max_value=0.5, step=0.1)
-        )
-    )
+    # Dropout layer for regularization
+    hp_dropout = hp.Float("dropout", 0.2, 0.5)
+    model.add(keras.layers.Dropout(rate=hp_dropout))
 
-    # 1x1 Conv2D to reduce feature map to num_classes
+    # 1x1 2D Convolutional layer to reduce feature map
     model.add(
         keras.layers.Conv3D(
-            filters=num_classes,
+            filters=dim_alt,
             kernel_size=(1, 1, 1),
             activation="linear",
             padding="same",
@@ -72,57 +70,25 @@ def model_initializer(hp, frame_size: int = 9):
     )
 
     # Compile the model
+    hp_learning_rate = hp.Choice("learning_rate", values=[1e-4, 1e-3, 1e-2])
     model.compile(
+        optimizer=keras.optimizers.Adam(learning_rate=hp_learning_rate),
         loss=keras.losses.MeanSquaredError(),
-        optimizer=keras.optimizers.Adam(
-            learning_rate=hp.Float(
-                "learning_rate", min_value=1e-4, max_value=1e-2, sampling="LOG"
-            )
-        ),
         metrics=[keras.metrics.MeanAbsoluteError()],
     )
 
-    # model.summary()
     return model
-
-
-def run_hyperparameter_tuning(
-    train_dataset: keras.utils.Sequence,
-    val_dataset: keras.utils.Sequence,
-):
-    tuner = kt.Hyperband(
-        model_initializer,
-        objective="val_loss",  # Minimize validation loss
-        max_epochs=10,  # Maximum number of epochs per trial
-        factor=3,  # The factor by which the number of trials decreases
-        directory="kt_tuning",  # Directory to save results
-        project_name="hyperparameter_tuning",
-    )
-
-    print("About to do the tuner search")
-    tuner.search(train_dataset, epochs=10, validation_data=val_dataset, verbose=2)
-    print("a best model has been retrieved")
-    best_hyperparameters = tuner.get_best_hyperparameters(num_trials=1)[
-        0
-    ].hyperparameters
-    print(best_hyperparameters.values)
-    best_model = tuner.get_best_models(num_models=1)
-    print(best_model[0].summary())
-    return best_model
 
 
 if __name__ == "__main__":
     multiprocessing.set_start_method("forkserver", force=True)
+    rng = rnd.default_rng(seed=42)
+
+    # Generate dataset timestamps
     timestamps = generate_timestamps(
         start=dt.datetime(2024, 11, 6, 0, 0, tzinfo=dt.UTC),
         end=dt.datetime(2024, 11, 6, 12, 0, tzinfo=dt.UTC),
     )
-    # print([len(sequence) for sequence in timestamps])
-    # print([sequence[0] for sequence in timestamps])
-    # print("\n\n\n\n")
-    # print([sequence[90] for sequence in timestamps])
-
-    rng = rnd.default_rng(seed=42)
     rng.shuffle(timestamps)
     t_train, t_val = timestamps[:10], timestamps[10:]
     t_test = generate_timestamps(
@@ -130,47 +96,44 @@ if __name__ == "__main__":
         end=dt.datetime.now(tz=dt.UTC),
     )
 
-    print(f"Length of train dataset: {len(t_train)} with {sum(map(lambda x: len(x), t_train))} total elements")
-    print(f"Length of val dataset: {len(t_val)} with {sum(map(lambda x: len(x), t_val))} total elements")
-    print(f"Length of test dataset: {len(t_test)} with {sum(map(lambda x: len(x), t_test))} total elements")
+    print(f"Timestamps: {len(t_train)}/{len(t_val)}:{len(t_test)}")
 
     sat = GOES(satellite=16, product="ABI", domain="C")
     bands = [8, 9, 10, 13, 14, 15]
 
-    train_generator = Generator(
+    # Create the generators
+    gen_train = Generator(
         t_train, batch_size=1, frame_size=9, sat=sat, bands=bands
     )  # xs shape: (4,1500,2500, 6) ys shape: (4,1500,2500, 14)
-    val_generator = Generator(
+    gen_val = Generator(
         t_val, batch_size=1, frame_size=9, sat=sat, bands=bands
     )  # xs shape: (4,1500,2500, 6) ys shape: (4,1500,2500, 14)
-    test_generator = Generator(
+    gen_test = Generator(
         t_test, batch_size=1, frame_size=9, sat=sat, bands=bands
     )  # xs shape: (4,1500,2500, 6) ys shape: (4,1500,2500, 14)
 
-    print("train batches: ", len(train_generator))
-    print("val batches: ", len(val_generator))
-    print("test batches: ", len(test_generator))
+    print(f"Generators: {len(gen_train)}/{len(gen_val)}:{len(gen_test)}")
 
-    # Code to test the model initializer
-    # test_hp = kt.HyperParameters()
-    # test_model = model_initializer(test_hp, frame_size=9)
-    # test_model.summary()
-
-    print("About to run the hyperparameter tuning loop")
-    best_model = run_hyperparameter_tuning(train_generator, val_generator)
-    best_model.summary()
-
-    checkpoint_path = "persistent_files/best_model_checkpoint.h5"
-    checkpoint_callback = keras.callbacks.ModelCheckpoint(
-        filepath=checkpoint_path,
-        monitor="val_loss",  # Monitor validation loss
-        save_best_only=True,  # Save only the best model
-        save_weights_only=False,  # Save entire model (structure + weights)
-        verbose=1,
+    # Tune the model
+    tuner = kt.Hyperband(
+        build_model,
+        objective="val_loss",
+        max_epochs=10,
+        factor=3,
+        directory="tuning",
+        project_name="turbulent",
     )
 
-    best_model.fit(
-        train_generator, epochs=10, checkpoint_callback=[checkpoint_callback]
-    )
-    # final_loss, final_mae = best_model.evaluate(X_test, y_test)
-    # print(f"Test Loss: {final_loss}, Test MAE: {final_mae}")
+    # Run the hyperparameter search
+    tuner.search(gen_train, validation_data=gen_val, epochs=10)
+
+    # Get the optimal hyperparameters
+    best_hps = tuner.get_best_hyperparameters()[0]
+    print(best_hps)
+
+    model = tuner.hypermodel.build(best_hps)
+    history = model.fit(gen_train, validation_data=gen_val, epochs=50)
+
+    val_loss_per_epoch = history.history["val_loss"]
+    best_epoch = val_loss_per_epoch.index(max(val_loss_per_epoch)) + 1
+    print("Best epoch: %d" % (best_epoch,))
