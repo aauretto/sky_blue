0506d5878042dc16bc56a361398304edbce6f7dd
Simon M. Webber <swebbe01@login-prod-03.pax.tufts.edu>
2025-04-03 12:41:00 -0400

    starting branch

---
+++ src/model_ex_machina.py
@@ -0,0 +1,135 @@
+from Logger import LOGGER
+import datetime as dt
+import multiprocessing
+
+import keras
+import keras_tuner as kt
+from numpy import random as rnd
+from goes2go import GOES
+
+import consts as consts
+from generator import Generator
+import pickle
+import os
+
+
+def generate_timestamps(
+    start: dt.datetime = dt.datetime(2017, 3, 1, 0, 3, tzinfo=dt.UTC),
+    end: dt.datetime = dt.datetime(2025, 1, 1, 0, 0, tzinfo=dt.UTC),
+) -> list[dt.datetime]:
+    """
+    Generates a list of 5 minutes seperated datetimes starting on minute 3
+    of each year 2018-2024 and 2017 without Jan and Feb
+
+    Returns
+    -------
+    a list of datetimes in the range
+    """
+
+    timestamps = [[] for _ in range(12)]
+    current_time = start
+
+    while current_time < end:
+        timestamps[current_time.minute // 5].append(current_time)
+        current_time = current_time + dt.timedelta(minutes=5)
+
+    return timestamps
+
+
+def build_model(
+    hp: kt.HyperParameters,
+    dim_frames=9,
+    dim_lat=consts.GRID_RANGE["LAT"],
+    dim_lon=consts.GRID_RANGE["LON"],
+    dim_alt=consts.GRID_RANGE["ALT"],
+    dim_bands=6,
+):
+    model = keras.Sequential()
+    model.add(keras.layers.Input((dim_frames, dim_lat, dim_lon, dim_bands)))
+
+    # 2D Convolutional LSTM layer
+    # hp_filters = hp.Choice("filters", values=[8, 16, 32])
+    model.add(
+        keras.layers.ConvLSTM2D(
+            filters=16, #Playing god and choosing #TODO
+            kernel_size=(3, 3), # Todo may need to become 5x5
+            padding="same",
+            return_sequences=True,
+        )
+    )
+
+    # Dropout layer for regularization
+    # hp_dropout = hp.Float("dropout", 0.2, 0.5)
+    model.add(keras.layers.Dropout(rate=.25)) # Playing god and choosing #TODO
+
+    # 1x1 2D Convolutional layer to reduce feature map
+    model.add(
+        keras.layers.Conv3D(
+            filters=dim_alt,
+            kernel_size=(1, 1, 1),
+            activation="linear",
+            padding="same",
+        )
+    )
+
+    # Compile the model
+    # hp_learning_rate = hp.Choice("learning_rate", values=[1e-4, 1e-3, 1e-2])
+    model.compile(
+        optimizer=keras.optimizers.Adam(learning_rate=1e-3), # Playing god a choosing #TODO
+        loss=keras.losses.MeanSquaredError(),
+        metrics=[keras.metrics.MeanAbsoluteError()],
+    )
+
+    return model
+
+
+if __name__ == "__main__":
+    multiprocessing.set_start_method("forkserver", force=True)
+    rng = rnd.default_rng(seed=42)
+
+    # Generate dataset timestamps
+    timestamps = generate_timestamps(
+        start=dt.datetime(2024, 1, 1, 0, 0, tzinfo=dt.UTC),
+        end=dt.datetime(2024, 1, 1, 10, 0, tzinfo=dt.UTC)
+    )
+    rng.shuffle(timestamps)
+    t_train, t_val = timestamps[:10], timestamps[10:]
+    # t_test = generate_timestamps(
+    #     start=dt.datetime(2025, 1, 1, 0, 0, tzinfo=dt.UTC),
+    #     end=dt.datetime(2025, 2, 1, 0, 0)
+    # )
+
+    # print(f"Timestamps: {len(t_train)}/{len(t_val)}:{len(t_test)}")
+
+    sat = GOES(satellite=16, product="ABI", domain="C")
+    bands = [8, 9, 10, 13, 14, 15]
+
+    # Create the generators
+    gen_train = Generator(
+        t_train, batch_size=1, frame_size=9, sat=sat, bands=bands
+    )  # xs shape: (4,1500,2500, 6) ys shape: (4,1500,2500, 14)
+    gen_val = Generator(
+        t_val, batch_size=1, frame_size=9, sat=sat, bands=bands
+    )  # xs shape: (4,1500,2500, 6) ys shape: (4,1500,2500, 14)
+    # gen_test = Generator(
+    #     t_test, batch_size=1, frame_size=9, sat=sat, bands=bands
+    # )  # xs shape: (4,1500,2500, 6) ys shape: (4,1500,2500, 14)
+
+    # print(f"Generators: {len(gen_train)}/{len(gen_val)}:{len(gen_test)}")
+
+
+    model = build_model(None)
+    history = model.fit(gen_train, validation_data=gen_val, epochs=2) # epochs: 50
+
+    SAVE_PATH = '/cluster/tufts/capstone25skyblue/model_saves'
+    fullTimestamp = dt.datetime.now().strftime("%Y_%m_%d/%H_%M_%S")
+    [yymmdd, hhmmss] = fullTimestamp.split("/")
+
+    SAVE_DIR = SAVE_PATH + '/' + yymmdd
+    os.makedirs(SAVE_DIR, exist_ok = True)
+    MODEL_SAVE_PATH = SAVE_DIR + f"/{hhmmss}_model.keras"
+    HISTORY_SAVE_PATH = SAVE_DIR + f"{hhmmss}_history.pkl"
+
+    model.save(MODEL_SAVE_PATH)
+    with open(HISTORY_SAVE_PATH, 'wb') as f:
+        pickle.dump(history.history, f)
