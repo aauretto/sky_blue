0a7a65e36cd485d1074a13815d9622fa4d77799e
Simon M. Webber <swebbe01@login-prod-01.pax.tufts.edu>
2025-04-03 13:18:41 -0400

    fixed timestamps on fetch

--- src/satellite/__init__.py
+++ src/satellite/__init__.py
@@ -8,7 +8,7 @@ from xarray import DataArray, Dataset
 
 def fetch(timestamp: dt.datetime, satellite: GOES) -> Dataset:
     return satellite.nearesttime(
-        dt.datetime.fromtimestamp(timestamp.timestamp(), tz=None), # Omitting timezone intentionally so we get a niave object
+        timestamp.replace(tzinfo=None),
         return_as="xarray",
         download=False,
         verbose=False
