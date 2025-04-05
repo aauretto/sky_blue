5947bfa969fb18e26e1b6450c9adc1761a0dc2d2
Aiden Auretto <aiden.auretto@tufts.edu>
2025-04-04 21:44:54 -0400

    Fleshed out sat cache

--- src/satellite_cache.py
+++ src/satellite_cache.py
@@ -10,117 +10,147 @@ from concurrent.futures import ThreadPoolExecutor
 import os
 import xarray as xr
 
+### MEGA CONSTANTS:
+MAX_THREADS = 1280
+WINDOW_PER_HOUR = 12
 BANDS = [8,9,10,13,14,15]
 CACHE_DIR = "/skyblue/caching"
 
+
 def parse_timestamp(ts):
+    """
+    Parse a datetime.datetime and return a formatted strings
+
+    Parameters
+    ----------
+    ts: datetime.datetime
+
+    Returns
+    -------
+    list(str)
+        With the format [YYYY_MM_DD, HH_MM_SS]
+    """
     return ts.strftime("%Y_%m_%d|%H_%M_%S").split("|")
 
 SAT = GOES(satellite=16, product="ABI", domain="C")
-def download_images(timestamps):
-    def worker(ts):
-        startTime = time.time()
-        yyyymmdd, hhmmss = parse_timestamp(ts)
-        thisDir = f"{CACHE_DIR}/{yyyymmdd}"
-        outfile = f"{thisDir}/{hhmmss}.npz"
 
-        rawXarr = st.fetch(ts, SAT)
-        lats, lons = st.calculate_coordinates(rawXarr)
 
-        wTime = xr.concat([rawXarr], dim="t")
-        updated_timestamp = [ # we only want one timestamp here
-            dt.datetime.fromtimestamp(
-                timestamp.astype("datetime64[s]").astype(int), dt.UTC
-            )
-            for timestamp in np.array(wTime.coords["t"], dtype=np.datetime64)
-        ][0]
-        trimmed = st.fetch_bands(wTime, BANDS)
-        smoothed = st.smooth(st.project(lats, lons, trimmed.data))
-        os.makedirs(thisDir, exist_ok=True)
-        np.savez(outfile, image=smoothed, timestamp=updated_timestamp)
-        endTime = time.time()
-        print(f"Saved to {outfile} [{endTime-startTime:.4f}s]")
+# Worker function that will download a single GOES-16 image and save it with an updated timstamp
+# in the cache
+def cache_worker(ts):
+    """
+    Downloads the GOES-16 image nearest to the timestamp provided
+    Stores the result to a file in CACHE_DIR/yyyy_MM_DD/HH_MM_SS.npz
+    
+    Parameters
+    ----------
+    timestamp: datetime.datetime
+        The expected tzinfo is UTC
+    
+    Return
+    ------
+    None
+    """
+    # Figure out where we are going to store the cached file
+    yyyymmdd, hhmmss = parse_timestamp(ts)
+    thisDir = f"{CACHE_DIR}/{yyyymmdd}"
+    outfile = f"{thisDir}/{hhmmss}.npz"
+
+    if os.path.isfile(outfile):
+        return
+
+    # Get data from AWS, compute updated timestamp for data, pull out bands we want
+    rawXarr = st.fetch(ts, SAT)
+    lats, lons = st.calculate_coordinates(rawXarr)
+
+    # To cooperate with contract for fetch_bands, project, and smooth
+    wTime = xr.concat([rawXarr], dim="t")
+    updated_timestamp = [ # we only want one timestamp here
+        dt.datetime.fromtimestamp(
+            timestamp.astype("datetime64[s]").astype(int), dt.UTC
+        )
+        for timestamp in np.array(wTime.coords["t"], dtype=np.datetime64)
+    ][0]
+    trimmed = st.fetch_bands(wTime, BANDS)
+    smoothed = st.smooth(st.project(lats, lons, trimmed.data))
+    
+    # Write file to disk
+    os.makedirs(thisDir, exist_ok=True)
+    np.savez(outfile, image=smoothed, timestamp=updated_timestamp)
  
 
-    numProcs = len(timestamps)
-    with ThreadPoolExecutor(max_workers=numProcs) as exec:
-        xs = exec.map(worker, timestamps)
+def cache_images_from_aws(timestamps):
+    """
+    Downloads the GOES-16 image nearest to each timestamp in the provided
+    list of timestamps.
+    Stores the results to a files in CACHE_DIR
+    
+    Parameters
+    ----------
+    timestamps: list(datetime.datetime)
+        The expected tzinfo is UTC
+    
+    Return
+    ------
+    None
+    """
+    # Launch job to download all timestamps over multiple threads
+    with ThreadPoolExecutor(max_workers=MAX_THREADS // WINDOW_PER_HOUR) as exec:
+        xs = exec.map(cache_worker, timestamps)
     
 
-def load_images(tsList):
+def retreive_satellite_data(tsList):
+    """
+    Attempts to pull the requested satellite data from the cache and if it
+    does not exist, creates a cache file for it and then pulls it
+
+    Parameters
+    ----------
+    tsList: list(datetime.datetime)
+        Expects the tzinfo to be UTC and will retrieve a file with with exactly
+        that timestamp
+        
+    Returns
+    -------
+    (np.ndarray, list(datetime.datetime))
+    The first element in the tuple is a numpy array of shape (len(tsList), sat_lats, sat_lons, sat_bands=6)
+    The second element is a list of the actual timestamps at which those images where taken (in UTC)
+
+    Thus, arr[0] occurred at timestampList[0]
     
-    def worker(ts):
-        startTime = time.time()
+    """
+    def retrieve_worker(ts):
+        
+        # Where to store the file
         yyyymmdd, hhmmss = parse_timestamp(ts)
         infile = f"{CACHE_DIR}/{yyyymmdd}/{hhmmss}.npz"
-        data = np.load(infile, allow_pickle = True)
-        endTime = time.time()
+        
+        # Try and get image from cache. If we can't, download it from aws first
+        try:
+            data = np.load(infile, allow_pickle = True)
+        except FileNotFoundError:
+            cache_worker(ts)
+            data = np.load(infile, allow_pickle = True)
 
-        print(f"Loaded from {infile} [{endTime-startTime:.4f}s]")
         # timestamp gets wrapped in a numpy object on save so we call .item to unbox it
         return data["timestamp"].item(), data["image"]
 
-    numProcs = len(tsList)
-    with ThreadPoolExecutor(max_workers=numProcs) as exec:
-        stampsAndImages = exec.map(worker, tsList)
+    with ThreadPoolExecutor(max_workers=MAX_THREADS // WINDOW_PER_HOUR) as exec:
+        stampsAndImages = exec.map(retrieve_worker, tsList)
 
     # Get images and stamps in their own arrays, merge the images
     [allStamps, allImages] = list(zip(*list(stampsAndImages)))
     return (np.vstack(allImages), list(allStamps))
 
-# Code from generator to test against
-def sanity_check(timestamps: list[dt.datetime]):
-        def worker(ts):
-            return st.fetch(ts, SAT)
-        
-        numProcs = len(timestamps)
-        with ThreadPoolExecutor(max_workers=numProcs) as exec:
-            xs = exec.map(worker, timestamps)
-
-        # xs = [st.fetch(t, self.sat) for t in timestamps]
-
-        xs = xr.concat(xs, dim="t")
-        lats, lons = st.calculate_coordinates(xs)
-        xs = st.fetch_bands(xs, BANDS)
-        updated_timestamps = [
-            dt.datetime.fromtimestamp(
-                timestamp.astype("datetime64[s]").astype(int), dt.UTC
-            )
-            for timestamp in np.array(xs.coords["t"], dtype=np.datetime64)
-        ]
-        xs = st.smooth(st.project(lats, lons, xs.data))
-        return xs, updated_timestamps
 
 if __name__ =='__main__':
-
-    print("CPU count (multiprocessing):", multiprocessing.cpu_count())
-
     multiprocessing.set_start_method("forkserver", force=True)
 
-    ts = dt.datetime(2020, 1, 1, 0, 0)
-    delts = dt.timedelta(hours = 1)
-    tsList = [ts + delts * i for i in range(9)]
-    print(*tsList, sep = "\n")
+    startTime = dt.datetime(2024, 1, 1, 0, 0, tzinfo=dt.UTC)
+    endTime = dt.datetime(2024, 1, 11, 0, 0, tzinfo=dt.UTC)
+
+    tsList = st.generate_timestamps(startTime, endTime) 
     
-    start = time.time()
-    download_images(tsList)
-    end = time.time()
-    print(f"Full caching for 9 procs took {end - start}s")
 
-    start = time.time()
-    ims, times = load_images(tsList)
-    end = time.time()
-
-    print(f"Full retreival for images took {end - start}s")
-
-    print("="*100)
-    print("Checking against known info:")
-
-    trueData, trueStamps = sanity_check(tsList)
-    print(type(trueStamps[0]))
-    print(type(times[0]))
-    # Check that the array we get from the old method and the array we get from the new method are the same
-    # print(trueData == ims)
-
-
-
+    with ThreadPoolExecutor(max_workers=WINDOW_PER_HOUR) as exec:
+        exec.map(cache_images_from_aws, tsList)
